836443b64f2af4a879578f2441aa9d0421e0a32c
Viraj Parikh <virajxp1@utexas.edu>
2020-12-16 18:53:45 -0600

    Finished project0a
    This project is a simple bfs search using the imdb database to find degrees of seperation between actors

---
+++ Project 0/degrees/degrees.py
@@ -0,0 +1,186 @@
+import csv
+from queue import Queue
+import sys
+
+from util import Node, StackFrontier, QueueFrontier
+
+# Maps names to a set of corresponding person_ids
+names = {}
+
+# Maps person_ids to a dictionary of: name, birth, movies (a set of movie_ids)
+people = {}
+
+# Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
+movies = {}
+
+
+def load_data(directory):
+    """
+    Load data from CSV files into memory.
+    """
+    # Load people
+    with open(f"{directory}/people.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            people[row["id"]] = {
+                "name": row["name"],
+                "birth": row["birth"],
+                "movies": set()
+            }
+            if row["name"].lower() not in names:
+                names[row["name"].lower()] = {row["id"]}
+            else:
+                names[row["name"].lower()].add(row["id"])
+
+    # Load movies
+    with open(f"{directory}/movies.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            movies[row["id"]] = {
+                "title": row["title"],
+                "year": row["year"],
+                "stars": set()
+            }
+
+    # Load stars
+    with open(f"{directory}/stars.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            try:
+                people[row["person_id"]]["movies"].add(row["movie_id"])
+                movies[row["movie_id"]]["stars"].add(row["person_id"])
+            except KeyError:
+                pass
+
+
+def main():
+    # if len(sys.argv) > 2:
+    #     sys.exit("Usage: python degrees.py [directory]")
+    # directory = sys.argv[1] if len(sys.argv) == 2 else "large"
+    directory = "large"
+
+    # Load data from files into memory
+    print("Loading data...")
+    load_data(directory)
+    print("Data loaded.")
+
+    source = person_id_for_name(input("Name: "))
+    if source is None:
+        sys.exit("Person not found.")
+    target = person_id_for_name(input("Name: "))
+    if target is None:
+        sys.exit("Person not found.")
+
+    path = shortest_path(source, target)
+
+    if path is None:
+        print("Not connected.")
+    else:
+        degrees = len(path)
+        print(f"{degrees} degrees of separation.")
+        path = [(None, source)] + path
+        for i in range(degrees):
+            person1 = people[path[i][1]]["name"]
+            person2 = people[path[i + 1][1]]["name"]
+            movie = movies[path[i + 1][0]]["title"]
+            print(f"{i + 1}: {person1} and {person2} starred in {movie}")
+
+
+def shortest_path(source, target):
+    """
+    Returns the shortest list of (movie_id, person_id) pairs
+    that connect the source to the target.
+
+    If no possible path, returns None.
+    """
+
+    """
+    right now we have verified it works with 1 degree of seperation
+    create an example with 2 degrees of seperation and see if algorithm holds
+    then do an example with as far seperation as you can go in the small database 
+    expand to large database and do some testing
+    turn in 
+    """""
+
+    found = False
+    sourceNode = Node(source, None, None)
+    actor_viewed = set()
+    actor_viewed.add(source)
+    frontier = Queue(0)
+    frontier.put(sourceNode)
+    actions = []
+    while not frontier.empty():
+        if found: break
+        current = frontier.get()
+        current_id = current.state
+        actorList = list(neighbors_for_person(current_id))
+        for movie, actor in actorList:
+            a = Node(actor, current, action=(movie, actor))
+            if actor == target:
+                actions.append(a)
+                found = True
+                break
+            if actor not in actor_viewed:
+                frontier.put(a)
+                actions.append(a)
+                actor_viewed.add(actor)
+
+    path = []
+    if(len(actions) == 0): return None
+    i = len(actions)-1
+    if(actions[i].state != target): return None
+
+    while True:
+        current = actions[i]
+        path.append(current.action)
+        prev = current.parent
+        if(prev.state == sourceNode.state):
+            break
+        else:
+            i = actions.index(prev)
+
+    path.reverse()
+    return path
+
+
+def person_id_for_name(name):
+    """
+    Returns the IMDB id for a person's name,
+    resolving ambiguities as needed.
+    """
+    person_ids = list(names.get(name.lower(), set()))
+    if len(person_ids) == 0:
+        return None
+    elif len(person_ids) > 1:
+        print(f"Which '{name}'?")
+        for person_id in person_ids:
+            person = people[person_id]
+            name = person["name"]
+            birth = person["birth"]
+            print(f"ID: {person_id}, Name: {name}, Birth: {birth}")
+        try:
+            person_id = input("Intended Person ID: ")
+            if person_id in person_ids:
+                return person_id
+        except ValueError:
+            pass
+        return None
+    else:
+        return person_ids[0]
+
+
+def neighbors_for_person(person_id):
+    """
+    Returns (movie_id, person_id) pairs for people
+    who starred with a given person.
+    """
+    movie_ids = people[person_id]["movies"]
+    neighbors = set()
+    for movie_id in movie_ids:
+        for person_id in movies[movie_id]["stars"]:
+            neighbors.add((movie_id, person_id))
+    return neighbors
+
+
+if __name__ == "__main__":
+    main()
